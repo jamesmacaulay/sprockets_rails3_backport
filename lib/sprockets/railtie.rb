91eac3f34f1372c0c97fcb3c45ab37dbc86d8d0d
James MacAulay <james@shopify.com>
2011-12-13 17:17:40 -0500

    cleanup

--- lib/sprockets/railtie.rb
+++ lib/sprockets/railtie.rb
@@ -9,23 +9,6 @@ module Sprockets
 
   # TODO: Get rid of config.assets.enabled
   class Railtie < ::Rails::Railtie
-    # # config.action_controller.default_asset_host_protocol = :relative
-    # config.assets = ActiveSupport::OrderedOptions.new
-    # # config.assets.enabled                  = false
-    # config.assets.paths                    = []
-    # config.assets.precompile               = [ Proc.new{ |path| !['.js', '.css'].include?(File.extname(path)) },
-    #                                            /(?:\/|\\|\A)application\.(css|js)$/ ]
-    # config.assets.prefix                   = "/assets"
-    # config.assets.version                  = ''
-    # config.assets.debug                    = false
-    # config.assets.compile                  = true
-    # config.assets.digest                   = false
-    # config.assets.manifest                 = nil
-    # config.assets.cache_store              = false
-    # config.assets.js_compressor            = nil
-    # config.assets.css_compressor           = nil
-    # config.assets.initialize_on_precompile = true
-
     rake_tasks do
       load "sprockets/assets.rake"
     end
